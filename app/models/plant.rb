80a87ed983114c4284c93c0abacc26a1543b8ef1
Tylor Dodge <tylor.dodge1@gmail.com>
2016-06-03 19:46:01 -0400

    inverse friends showing

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,6 @@
 class Plant < ActiveRecord::Base
   has_many :friendships
   has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :plant
 end
