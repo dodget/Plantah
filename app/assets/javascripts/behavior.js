84dbc08818e5a490de5285e365297c1f213a2cd3
Tylor Dodge <tylor.dodge1@gmail.com>
2016-06-24 18:47:03 -0400

    added jquery animation to jumbotron plant list search

---
+++ app/assets/javascripts/behavior.js
@@ -0,0 +1,20 @@
+//all javascript behavior
+
+$(document).ready(function(){
+
+
+
+  $('#indexSearch').click(function(){
+    transformSearch1();
+  });
+
+
+});
+
+
+function transformSearch1(){
+  $('#jumbotron-phrase').slideUp();
+  setTimeout(function(){
+    $('#search').slideDown();
+  }, 1000)
+}
